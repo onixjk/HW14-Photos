1bd103ed3759f4447705e9ffa0cce30158d6eda0
onixjk <onixjkk@gmail.com>
2022-07-24 14:15:15 +0300

    Setup Tab bar.
    Create Albums, ForYou, Library, Search view controllers.
    Setup Tab bar.
    Setup Window.

---
+++ HW14-Photos/Source/Metric.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  HW14-Photos
+//
+//  Created by Onix qq on 24.07.22.
+//
+
+enum Strings {
+    static let libraryTitle = "Медиатека"
+    static let forYouTitle = "Для Вас"
+    static let albumTitle = "Альбомы"
+    static let searchTitle = "Поиск"
+    
+    static let libraryIcon = "photo.on.rectangle"
+    static let forYouIcon = "doc.richtext"
+    static let albumIcon = "rectangle.stack"
+    static let searchIcon = "magnifyingglass"
+}
