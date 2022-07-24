e91a524acedeb8a43c69786e2e8620cf7baa3921
onixjk <onixjkk@gmail.com>
2022-07-24 13:44:14 +0300

    Initial Commit

---
+++ HW14-Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW14-Photos
+//
+//  Created by Onix qq on 24.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
